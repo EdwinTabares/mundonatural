deb17e6e2d402a7939c9b73578766459a200e610
Edwin Tabares <edwin_rdrg@hotmail.com>
2017-10-04 17:49:27 -0500

    ultimo commit de hoy
    models, auth

--- app/User.php
+++ app/User.php
@@ -14,9 +14,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['name', 'email', 'password','type'];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -24,6 +22,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+    'password', 'remember_token',
     ];
+}
+
+public function products()
+{
+    return $this->hasMany('App\Product');
 }
